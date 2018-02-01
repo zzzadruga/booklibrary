58a2781f352cccc16cc0f0912243312f8f8ed0bc
zzzadruga <zzzadruga@gmail.com>
2018-02-01 13:34:54 +0300

    Editing model for working with hibernate

--- src/main/java/ru/zzzadruga/model/Book.java
+++ src/main/java/ru/zzzadruga/model/Book.java
@@ -2,9 +2,17 @@ package ru.zzzadruga.model;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Data
 @AllArgsConstructor
+@Entity(name = "Book")
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String title;
     private String description;
