27198f1a69a144e44e3ad6fcbd08f155dbea4529
zzzadruga <zzzadruga@gmail.com>
2018-02-01 17:28:28 +0300

    Add another methods from CRUD in interface BookDAO

--- src/main/java/ru/zzzadruga/dao/BookDAO.java
+++ src/main/java/ru/zzzadruga/dao/BookDAO.java
@@ -5,5 +5,9 @@ import ru.zzzadruga.model.Book;
 import java.util.List;
 
 public interface BookDAO {
+    void addBook();
+    void updateBook();
     List<Book> list();
+    Book getBookById(int id);
+    void removeBook(int id);
 }
