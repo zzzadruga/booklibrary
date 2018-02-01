90c8c3c2b0715b0c9161ae361fb1b9425866b2de
zzzadruga <zzzadruga@gmail.com>
2018-02-01 13:13:25 +0300

    Add some annotation

--- src/main/java/ru/zzzadruga/controller/BookController.java
+++ src/main/java/ru/zzzadruga/controller/BookController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import ru.zzzadruga.model.Book;
 import ru.zzzadruga.service.BookService;
 
@@ -16,6 +17,7 @@ public class BookController {
     private BookService bookService;
 
     @RequestMapping(value = "/list", method = RequestMethod.GET)
+    @ResponseBody
     public List<Book> getAllBooks(){
         return bookService.getAllBooks();
     }
--- src/main/java/ru/zzzadruga/service/BookServiceImpl.java
+++ src/main/java/ru/zzzadruga/service/BookServiceImpl.java
@@ -1,11 +1,13 @@
 package ru.zzzadruga.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import ru.zzzadruga.dao.BookDAO;
 import ru.zzzadruga.model.Book;
 
 import java.util.List;
 
+@Service
 public class BookServiceImpl implements BookService{
 
     @Autowired
